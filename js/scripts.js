127da8be8b653c30307b2bae82f95e4deb0a2c71
astonevart <astonevart@gmail.com>
2017-03-08 20:54:47 +0200

    section-speakers

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,13 @@ $(function () {
     $('.collapse').on('hide.bs.collapse', function () {
         $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
     });
-    $('#myForm').validator();
+    $('#myForm').validator({
+        feedback: {
+            success: 'fa fa-check-circle',
+            error: 'fa fa-times-circle'
+        }
+    });
+    $('.form-sel').select2({
+        minimumResultsForSearch: Infinity
+    });
 });
